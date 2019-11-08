da8ca470ee46386104c764eefb5b59631fe84eca
Jacob <jacobduncan00@yahoo.com>
2019-11-08 10:27:10 -0500

    Adding files

---
+++ mystack.h
@@ -0,0 +1,12 @@
+#ifndef MYSTACK_H
+#define MYSTACK_H
+
+template <class T>
+class MyStack{
+  private:
+    struct Node{
+      T data;
+      Node* next;
+    };
+/* Fill in stack operations: push, pop, peek */
+}
